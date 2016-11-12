460f8f332eae5f2e7feba6fc52f7a0d90d7740df
sganeriwal <sganeriwal@paypal.com>
2016-11-13 05:12:23 +0530

    controllers added

--- nodejs/BeyondDonation/index.js
+++ nodejs/BeyondDonation/index.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var kraken = require('kraken-js');
-
+var dbconnector = require('./lib/dbconnector');
 
 var options, app;
 
@@ -25,4 +25,5 @@ app.use(kraken(options));
 app.on('start', function () {
     console.log('Application ready to serve requests.');
     console.log('Environment: %s', app.kraken.get('env:env'));
+    dbconnector.createConnection();
 });
