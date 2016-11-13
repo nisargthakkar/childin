76d370b7cee5a1da5ba353e2dc3dbafc6fc9c23a
Nisarg Thakkar <nisthakkar@paypal.com>
2016-11-13 08:30:33 +0530

    Adding some models

--- nodejs/BeyondDonation/models/etemplates.js
+++ nodejs/BeyondDonation/models/etemplates.js
@@ -4,9 +4,9 @@ var DBConnector = require('../lib/dbconnector');
 
 module.exports = {
     inserttemplates: function(templateName, templateStructure, callback) {
-        DBConnector.query("SELECT * FROM DONATION WHERE DonorId="+templateName, callback);
+        DBConnector.query("SELECT * FROM Donation WHERE DonorId="+templateName, callback);
     },
     getByTemplateName: function(templateName, callback) {
-        DBConnector.query("SELECT * FROM DONATION WHERE DonorId="+templateName, callback);
+        DBConnector.query("SELECT * FROM Donation WHERE DonorId="+templateName, callback);
     }
 }
--- nodejs/BeyondDonation/models/users/child.js
+++ nodejs/BeyondDonation/models/users/child.js
@@ -4,12 +4,12 @@ var DBConnector = require('../../lib/dbconnector');
 
 module.exports = {
     getChild: function(userid, callback) {
-        DBConnector.query("SELECT * FROM CHILD WHERE ID="+userid, callback);
+        DBConnector.query("SELECT * FROM Child WHERE ID="+userid, callback);
     },
     getAllChild: function(callback) {
-        DBConnector.query("SELECT * FROM CHILD", callback);;
+        DBConnector.query("SELECT * FROM Child", callback);;
     },
-    insertChild: function(userid, info, callback) {
-        DBConnector.query("INSERT INTO CHILD (id, name) VALUES (" + userid + ",'" + info + "\')", callback);
+    insertChild: function(Name, GenderType, MaritalStatusType, DOB, CreatedOn, isActive, callback) {
+        DBConnector.query("INSERT INTO Child (Name, GenderType, MaritalStatusType, DOB, CreatedOn, isActive) VALUES ('" + Name + "', '" + GenderType + "', '" + MaritalStatusType + "', '" + DOB + "', '" + CreatedOn + "', '" + isActive + "')", callback);
     }
 }
